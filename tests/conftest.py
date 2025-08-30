d7dba41c5910c42b12bfb824e8132e6d70aaf495
shuntacurosu <shuntacurosu@gmail.com>
2025-08-31 02:46:33 +0900

    Fix conftest.py to remove InputHandler dependency and focus on core fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 from tetris.env import TetrisEnv
 from tetris.core import TetrisBoard
 from tetris.renderer import CUIRenderer
-from tetris.input_handler import InputHandler
 
 @pytest.fixture
 def tetris_env():
@@ -22,10 +21,3 @@ def cui_renderer():
     """CUIRenderer インスタンスを提供するフィクスチャ"""
     renderer = CUIRenderer()
     return renderer
-
-@pytest.fixture
-def input_handler():
-    """InputHandler インスタンスを提供するフィクスチャ"""
-    handler = InputHandler()
-    yield handler
-    handler.stop()
